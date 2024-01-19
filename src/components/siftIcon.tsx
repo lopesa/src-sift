d7da3f021fdd876cd9a5e577b1cf85f9ed223012
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2024-01-19 11:30:33 -0400

    improving the sifter animation

--- src/components/siftIcon.tsx
+++ src/components/siftIcon.tsx
@@ -4,9 +4,9 @@ type SiftIconProps = {
 
 const SiftIcon = ({ className }: SiftIconProps) => {
   return (
-    <div className={`${className} w-8 h-8`}>
+    <div className={`${className} w-10 h-10`}>
       <div className="animate-wiggleLeft h-0.5 w-5/6 bg-zinc-400 rounded-full mx-auto"></div>
-      <div className="animate-wiggleRight mt-px h-0.5 w-5/6 bg-zinc-400 rounded-full mx-auto"></div>
+      <div className="animate-wiggleRight h-0.5 w-5/6 bg-zinc-400 rounded-full mx-auto"></div>
       <div className="w-full h-full flex justify-evenly items-start pt-1">
         <div className="animate-wiggleUp w-0.5 h-1/4 bg-amber-300 rounded-full"></div>
         <div className="animate-wiggleDown w-0.5 h-1/4 bg-amber-300 rounded-full"></div>
