5d8fa4faa0d0b5aa0a4a3a0abc71ab1b1c62fa85
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2024-01-29 11:39:53 -0400

    add a middleware to protect the /api/chatgpt route from access from other than own domain

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const headers = request?.headers;
+  const host = headers?.get("host");
+  if (host !== "srcsift.io" && host !== "www.srcsift.io") {
+    return NextResponse.json({ error: "error" }, { status: 500 });
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/api/chatgpt",
+};
